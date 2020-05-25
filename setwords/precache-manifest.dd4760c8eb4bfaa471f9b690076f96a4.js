ed4558335d8d66822be7415fa465822ae2cc734b
Hamish Wright <hamish.wright@mvfglobal.com>
2020-05-25 22:52:47 +0100

    add setwords game

---
+++ setwords/precache-manifest.dd4760c8eb4bfaa471f9b690076f96a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bd66db8e5834b19ec1ad75ace06b28c",
+    "url": "/setwords/index.html"
+  },
+  {
+    "revision": "cec1fc758bcd591742ff",
+    "url": "/setwords/static/css/main.9652e696.chunk.css"
+  },
+  {
+    "revision": "fcd370bd175ae1a1765a",
+    "url": "/setwords/static/js/2.3d7582eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/setwords/static/js/2.3d7582eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cec1fc758bcd591742ff",
+    "url": "/setwords/static/js/main.d1a626c0.chunk.js"
+  },
+  {
+    "revision": "dc4c5e8fa1a5f7e3aa68",
+    "url": "/setwords/static/js/runtime-main.f7b1b258.js"
+  }
+]);
